e57b8e552fc2b62961cc63fb0af4c5e842d80586
Ishirai <kirakiseki@163.com>
2024-08-18 22:07:39 +0800

    feat: availableGuilds

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export const name = 'imageprovider'
 interface ImageProviderEntry {
   message: string
   imageUrl: string
+  availableGuilds: string[]
 }
 
 export interface Config {
@@ -15,13 +16,15 @@ export const Config: Schema<Config> = Schema.object({
   entries: Schema.array(Schema.object({
     message: Schema.string().required().description('触发消息内容'),
     imageUrl: Schema.string().required().description('回复图片URL'),
-  })).description('消息与回复图片配置'),
+    availableGuilds: Schema.array(Schema.string()).default([]).description('生效群组列表'),
+  })).description('消息、回复图片、可用群组配置'),
 })
 
 export function apply(ctx: Context, config: Config) {
   ctx.on('message', (session) => {
     for (const entry of config.entries) {
       if (session.content === entry.message) {
+        if (entry.availableGuilds.length && !entry.availableGuilds.includes(session.guildId)) return
         session.send(h('img', { src: entry.imageUrl }))
         return
       }
