1bf910c3c6a19b3b68349a61ab0f4e0028845e05
Ishirai <kirakiseki@163.com>
2024-08-18 22:36:59 +0800

    feat: build

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export const Config: Schema<Config> = Schema.object({
     message: Schema.string().required().description('触发消息内容'),
     imageUrl: Schema.string().required().description('回复图片URL'),
     availableGuilds: Schema.array(Schema.string()).default([]).description('生效群组列表'),
-  })).description('消息、回复图片、可用群组配置'),
+  })).description('消息、回复图片、可用群组配置')
 })
 
 export function apply(ctx: Context, config: Config) {
